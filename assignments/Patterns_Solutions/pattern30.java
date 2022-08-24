e3cc04fe2ae807ffc1f924165747d811df790c88
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-08-24 19:16:41 +0530

    Adding pattern30.java

---
+++ assignments/Patterns_Solutions/pattern30.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/*
+PATTERN_30
+            1
+          2 1 2
+        3 2 1 2 3
+      4 3 2 1 2 3 4
+    5 4 3 2 1 2 3 4 5
+*/
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		pattern30(5);
+	}
+	public static void pattern30(int n)
+	{
+	    for(int row=1;row<=n;row++)
+	    {
+	        for(int space=0;space<n-row;space++)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int col=row;col>=1;col--)
+	        {
+	            System.out.print(col+" ");
+	        }
+	        for(int col=2;col<=row;col++)
+	        {
+	            System.out.print(col+" ");
+	        }
+	        System.out.println();
+	    }
+	    
+	}
+}
